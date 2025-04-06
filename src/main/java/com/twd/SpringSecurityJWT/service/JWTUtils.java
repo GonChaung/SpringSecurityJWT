fef29d1bc48767f72710a109fde733072540d164
Pandas-12 <thantsithu.naing@icloud.com>
2025-04-06 14:00:35 +0630

    first commit

---
+++ src/main/java/com/twd/SpringSecurityJWT/service/JWTUtils.java
@@ -0,0 +1,53 @@
+package com.twd.SpringSecurityJWT.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Component
+public class JWTUtils {
+    private SecretKey Key;
+    private static final long EXPIRATION_TIME = 86400000; //24 hrs or milliseconds
+
+    public JWTUtils(){
+        String secretString = "432452454554AKJKII997979HHHIE8989897IEEET44";
+        byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+        this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+    public String generatedToken(UserDetails userDetails){
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(Key)
+                .compact();
+    }
+
+    public String generatedRefreshToken(HashMap<String, Object> claims, UserDetails userDetails){
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(Key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims,T> claimsTFunction){
+        return claimsTFunction.apply(Jwts.parser().verifyWith(Key).build().parseSignedClaims(token).getPayload());
+    }
+}
